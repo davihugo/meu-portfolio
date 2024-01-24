685812ef37a89f5c01523c6d3f97b5f4dfe8bc01
davi.hugo <davihhugo05@gmail.com>
2024-01-24 00:14:15 -0300

    Descrição das alterações

---
+++ menu.js
@@ -0,0 +1,54 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+let formEnviar = document.querySelector('.btn-enviar input');
+
+// Função para redirecionar para o WhatsApp
+function enviarParaWhatsApp() {
+    var numeroWhatsApp = +5598981454184 ;
+    
+    var linkWhatsApp = "https://api.whatsapp.com/send?phone=" + +5598981454184;
+    
+    window.location.href = linkWhatsApp;
+}
+
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+});
+
+
+formEnviar.addEventListener('click', (event) => {
+    event.preventDefault();
+    enviarParaWhatsApp();
+});
+
+
+// Adiciona rolagem suave para os links de navegação
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+formEnviar.addEventListener('click', (event) => {
+    event.preventDefault();
+    enviarParaWhatsApp();
+});
